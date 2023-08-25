0c65e6919837f07428b14b18b3e99f460634d969
Your Name <you@example.com>
2023-08-25 16:11:09 -0300

    Continuação

--- src/edu/bernard/ex1/Aprendendo.java
+++ src/edu/bernard/ex1/Aprendendo.java
@@ -1,7 +1,6 @@
 package edu.bernard.ex1;
 public class Aprendendo {
-        //CÓDIGO VAI AQUI
-        //quando progrma precisa realizar alguma açao o codigo é inicializado com main:
+        
 
         public static void main(String [] args){
             String primeiroNome= "Bernard";
