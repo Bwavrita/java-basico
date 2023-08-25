c0e7a9642e950363c7834c0a4f4d99043441c595
Your Name <you@example.com>
2023-08-25 16:10:47 -0300

    Continuação

---
+++ src/edu/bernard/ex3/calculador.java
@@ -0,0 +1,30 @@
+package edu.bernard.ex3;
+
+public class calculador {
+    public static void main(String[] arg){
+        int subtracao= 50-10;
+        int adicao= 50+10;
+        int multiplicacao= 50*10;
+        int divisao= 50/10;
+        int resto= 3%2;
+        
+        System.out.println(resto);
+
+        String concatenacao=1+1+1+"1";
+
+        System.out.println(concatenacao);
+         
+        int a,b;
+
+        a=7;
+        b=6;
+        String resultado= a==b ? "verdadeiro": "falso";
+    
+
+        System.out.println(resultado);
+
+        boolean simNao = a == b;
+
+        System.out.println(simNao );
+    }
+}
